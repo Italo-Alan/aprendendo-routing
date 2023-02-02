79a092624fd86a71f863b3a8105d7642600c2f62
Italo-Alan <italoalan123@gmail.com>
2023-02-02 17:58:20 -0300

    :construction: Progredindo no conteúdo de routes em Angular

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 //Dentro desse array que declaramos todas as nossas rotas.
 const APP_ROUTES: Routes = [
   { path:'', component: HomeComponent},
+  //Caso seja necessário colocar um outro caminho que precise de um ID, faremos da seguinte forma:
   { path:'cursos', component: CursosComponent},
+  //Podemos dar qualquer nome desejado, os 2 pontos servem para identificar como um ID
+  { path:'curso/:id', component: CursoDetalheComponent},
   { path:'login', component: LoginComponent},
 ];
 
